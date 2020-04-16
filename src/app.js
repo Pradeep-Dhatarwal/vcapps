dbd7025cea5092ae780a7fe061368cd49b745234
pradeep-dhatarwal <pradsngh523@gmail.com>
2020-04-16 17:31:34 +0530

    bugfix

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ let io = require('socket.io')(server);
 let stream = require('./ws/stream');
 let path = require('path');
 let favicon = require('serve-favicon')
-
+let port = process.env.PORT || 3000;
 app.use(favicon(path.join(__dirname, 'favicon.ico')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
@@ -16,4 +16,6 @@ app.get('/', (req, res)=>{
 
 io.of('/stream').on('connection', stream);
 
-server.listen(3000);
+server.listen(port , () =>{
+    console.log("server running on port" + port)
+});
