3dbe70a7129ecbc03656f09163eb955ae5ee7935
pradeep-dhatarwal <pradsngh523@gmail.com>
2020-04-22 19:59:26 +0530

    added video elements

---
+++ app.js
@@ -0,0 +1,25 @@
+// express server
+let express = require('express');
+let app = express();
+
+
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+let stream = require('./ws/stream');
+let path = require('path');
+let favicon = require('serve-favicon')
+let port = process.env.PORT || 3000;
+
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
+
+
+io.of('/stream').on('connection', stream);
+
+server.listen(port , () =>{
+    console.log("server running on port" + port)
+});
--- src/app.js
+++ src/app.js
@@ -1,3 +1,36 @@
+// // express server
+// let express = require('express');
+// let app = express();
+// const https = require('https');
+
+// const fs = require('fs');
+// const key = fs.readFileSync('./key.pem');
+// const cert = fs.readFileSync('./cert.pem');
+// // const app = require("https-localhost")();
+// const server = https.createServer({key: key, cert: cert }, app);
+// // let server = require('http').Server(app);
+// let io = require('socket.io')(server);
+// let stream = require('./ws/stream');
+// let path = require('path');
+// let favicon = require('serve-favicon')
+// let port = process.env.PORT || 5000;
+
+// app.use(favicon(path.join(__dirname, 'favicon.ico')));
+// app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+// app.get('/', (req, res)=>{
+//     res.sendFile(__dirname+'/index.html');
+// });
+
+
+// io.of('/stream').on('connection', stream);
+
+// server.listen(port , () =>{
+//     console.log('server running on http://localhost:'+port)
+// });
+
+
+
 // express server
 let express = require('express');
 let app = express();
