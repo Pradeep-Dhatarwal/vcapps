16754b6600feac462433113f7629e1ed526f0bac
pradeep-dhatarwal <pradsngh523@gmail.com>
2020-04-24 13:00:37 +0530

    Added Chat

--- src/app.js
+++ src/app.js
@@ -1,51 +1,22 @@
-// express server
-let express = require('express');
-let app = express();
-const https = require('https');
-debugger;
-const fs = require('fs');
-var ssl = {
-    key:fs.readFileSync('./src/isotalkcerts/privateKey.pem', 'utf8'),
-    cert: fs.readFileSync('./src/isotalkcerts/isotalks.crt', 'utf8'),
-    ca: fs.readFileSync('./src/isotalkcerts/isotalksCA.crt', 'utf8')
-  };
-// const app = require("https-localhost")();
-const server = https.createServer( ssl , app);
-// let server = require('http').Server(app);
-let io = require('socket.io')(server);
-let stream = require('./ws/stream');
-let path = require('path');
-let favicon = require('serve-favicon')
-let port = process.env.PORT || 4433;
-
-app.use(favicon(path.join(__dirname, 'favicon.ico')));
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname+'/index.html');
-});
-
-
-io.of('/stream').on('connection', stream);
-
-server.listen(port , () =>{
-    console.log('server running on https://localhost:'+port)
-});
-
-
-
 // // express server
 // let express = require('express');
 // let app = express();
-
-
-// let server = require('http').Server(app);
+// const https = require('https');
+// debugger;
+// const fs = require('fs');
+// var ssl = {
+//     key:fs.readFileSync('./src/isotalkcerts/privateKey.pem', 'utf8'),
+//     cert: fs.readFileSync('./src/isotalkcerts/isotalks.crt', 'utf8'),
+//     ca: fs.readFileSync('./src/isotalkcerts/isotalksCA.crt', 'utf8')
+//   };
+// // const app = require("https-localhost")();
+// const server = https.createServer( ssl , app);
+// // let server = require('http').Server(app);
 // let io = require('socket.io')(server);
 // let stream = require('./ws/stream');
 // let path = require('path');
 // let favicon = require('serve-favicon')
-// let port = process.env.PORT || 3000;
-// const host="192.168.43.203";
+// let port = process.env.PORT || 4433;
 
 // app.use(favicon(path.join(__dirname, 'favicon.ico')));
 // app.use('/assets', express.static(path.join(__dirname, 'assets')));
@@ -57,6 +28,33 @@ server.listen(port , () =>{
 
 // io.of('/stream').on('connection', stream);
 
-// server.listen(port,host , () =>{
-//     console.log("server running on port" + port)
+// server.listen(port , () =>{
+//     console.log('server running on https://localhost:'+port)
 // });
+
+
+
+// // express server
+let express = require('express');
+let app = express();
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+let stream = require('./ws/stream');
+let path = require('path');
+let favicon = require('serve-favicon')
+let port = process.env.PORT || 3000;
+const host="192.168.43.203";
+
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
+
+
+io.of('/stream').on('connection', stream);
+
+server.listen(port,host , () =>{
+    console.log("server running on port" + port)
+});
