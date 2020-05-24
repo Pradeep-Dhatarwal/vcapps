6bca3f1b40944fc419ee92a976682b0002caecb0
pradeep singh <scavanger@pop-os.localdomain>
2020-05-25 01:57:01 +0530

    Added resizable whiteboard and timer

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,108 +2,34 @@
 
 (function() {
 
-  var socket = io();
-  var canvas = document.getElementsByClassName('whiteboard')[0];
-  var colors = document.getElementsByClassName('color');
-  var context = canvas.getContext('2d');
-var containerId=document.querySelector(".sketch-box");
-  var current = {
-    color: 'black'
-  };
-  var drawing = false;
+if(sessionStorage.seconds == undefined )
+{
+var totalSeconds = 0;
+sessionStorage.setItem("seconds", totalSeconds );
+} else{
+var totalSeconds = sessionStorage.getItem("seconds", totalSeconds );
+}
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
 
-  canvas.addEventListener('mousedown', onMouseDown, false);
-  canvas.addEventListener('mouseup', onMouseUp, false);
-  canvas.addEventListener('mouseout', onMouseUp, false);
-  canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
-  
-  //Touch support for mobile devices
-  canvas.addEventListener('touchstart', onMouseDown, false);
-  canvas.addEventListener('touchend', onMouseUp, false);
-  canvas.addEventListener('touchcancel', onMouseUp, false);
-  canvas.addEventListener('touchmove', throttle(onMouseMove, 10), false);
+setInterval(setTime, 1000);
+setInterval(()=>{
+  sessionStorage.setItem("seconds", totalSeconds );
+},5000)
+function setTime() {
+  ++totalSeconds;
 
-  for (var i = 0; i < colors.length; i++){
-    colors[i].addEventListener('click', onColorUpdate, false);
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
   }
-
-  socket.on('drawing', onDrawingEvent);
-
-  window.addEventListener('resize', onResize, false);
-  onResize();
-
-
-  function drawLine(x0, y0, x1, y1, color, emit){
-    context.beginPath();
-    let rect = containerId.getBoundingClientRect();
-    context.moveTo(x0-rect.left, y0-rect.top);
-    context.lineTo(x1-rect.left, y1-rect.top);
-    context.strokeStyle = color;
-    context.lineWidth = 3;
-    context.stroke();
-    context.closePath();
-
-    if (!emit) { return; }
-    var w = canvas.width;
-    var h = canvas.height;
-
-    socket.emit('drawing', {
-      x0: x0 / w,
-      y0: y0 / h,
-      x1: x1 / w,
-      y1: y1 / h,
-      color: color
-    });
-  }
-
-  function onMouseDown(e){
-    drawing = true;
-    current.x = e.clientX||e.touches[0].clientX;
-    current.y = e.clientY||e.touches[0].clientY;
-  }
-
-  function onMouseUp(e){
-    if (!drawing) { return; }
-    drawing = false;
-    drawLine(current.x, current.y, e.clientX||e.touches[0].clientX, e.clientY||e.touches[0].clientY, current.color, true);
-  }
-
-  function onMouseMove(e){
-    if (!drawing) { return; }
-    drawLine(current.x, current.y, e.clientX||e.touches[0].clientX, e.clientY||e.touches[0].clientY, current.color, true);
-    current.x = e.clientX||e.touches[0].clientX;
-    current.y = e.clientY||e.touches[0].clientY;
-  }
-
-  function onColorUpdate(e){
-    current.color = e.target.className.split(' ')[1];
-  }
-
-  // limit the number of events per second
-  function throttle(callback, delay) {
-    var previousCall = new Date().getTime();
-    return function() {
-      var time = new Date().getTime();
-
-      if ((time - previousCall) >= delay) {
-        previousCall = time;
-        callback.apply(null, arguments);
-      }
-    };
-  }
-
-  function onDrawingEvent(data){
-    var w = canvas.width;
-    var h = canvas.height;
-    drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
-  }
-
-  // make the canvas fill its parent
-  function onResize() {
-    canvas.width = document.querySelector(".sketch-box").offsetWidth;
-    canvas.height = document.querySelector(".sketch-box").offsetHeight;
-    //  canvas.width = window.innerWidth;
-    // canvas.height = window.innerHeight;
-  }
+}
 
 })();
